c9c364dcee62caa44c197cb86662bdff1434634c
Boris.Tuman <Boris.tuman@gmail.com>
2013-05-09 11:58:26 -0400

    Finished User signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Andy Yoder"
+    email    "amishboi@hotmail.com"
+    password "blackbuggy"
+    password_confirmation "blackbuggy"
+  end
+end
